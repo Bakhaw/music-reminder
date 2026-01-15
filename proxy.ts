9ac917750614908338b4d095adfe4207858f05f2
Hattou Faïssal <faissal.hattou@gmail.com>
2026-01-15 23:57:10 +0100

    chore: change middleware with proxy

--- proxy.ts
+++ proxy.ts
@@ -1,17 +1,18 @@
+"use server";
+
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export default async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-  });
-
+export default async function proxy(req: NextRequest) {
+  const token = await getToken({ req });
   const { pathname, origin } = req.nextUrl;
 
+  // Prevent authenticated users from visiting auth pages
   if ((pathname === "/sign-in" || pathname === "/sign-up") && token !== null) {
     return NextResponse.redirect(origin);
   }
 
+  // Allow auth routes or any authenticated request
   if (pathname.includes("api/auth") || token) {
     return NextResponse.next();
   }
