029e9bcdea1c2d10f4f6f603862f6a20ffe4b98f
Faissal HATTOU <faissal.hattou.ext@ACLF41WPY3PVY.(none)>
2026-01-17 18:30:11 +0100

    chore: update font settings causing build crash

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import Providers from "@/app/components/Providers";
 
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Music reminder",
@@ -28,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <main className="mx-auto w-full max-w-2xl h-screen">
           <Providers>
             <div className="h-full flex flex-col">{children}</div>
